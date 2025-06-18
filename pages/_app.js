313b1cc0a5c8aef319408f0d5c51659600b350cf
ThatMovieGuy <131075466+ThatMovieGuyOriginal@users.noreply.github.com>
2025-06-18 15:55:46 -0600

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@
 
 import '../styles/globals.css';
 import Head from 'next/head';
+import ErrorBoundary from '../components/ErrorBoundary';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -12,7 +13,9 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
     </>
   );
 }
